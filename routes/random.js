caa5bf909580ebc5fb8b93e873b08f83e48c7846
Santiago Quipildor <Quipildorsantiago@gmail.com>
2022-02-03 16:01:49 -0300

    env added

--- routes/random.js
+++ routes/random.js
@@ -9,11 +9,11 @@ routerRandom.get("/random", (req, res) => {
     const cant = parseInt(req.query.cant || 100000000);
   if (isNaN(cant)) {
     res.status(400).send({
-      error: "Parameter 'cant' must be a number",
+      msg: "debe ingresar un numero",
     });
     return;
   }
-  const random = fork("./src/utils/random.js", [cant]);
+  const random = fork("./utils/random.js", [cant]);
   random.on("message", (data) => {
     res.json({ iterations: cant, numbers: data });
   });
